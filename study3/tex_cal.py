d381cc037991f53aab06006e8e243f33a5cd7bc9
rosserGit <107463026+rosserGit@users.noreply.github.com>
2022-07-12 12:18:22 +0900

    tex_cal.py

--- study3/tex_cal.py
+++ study3/tex_cal.py
@@ -4,16 +4,16 @@
 # 물건 가격 = 소비자 가격 X 1/1.1
 # 부가세 = 물건 가격 X 0.1 = 소비자 가격 X 1/11
 
-while True:
-    pPrice = int(input('소비자 가격(원) : '))
-    vAnser = input('부가세 포함입니까? (y/n) : ')
 
-    if vAnser == 'y' or 'Y':
-        pValue = int(input('부가세(%) : '))
-        print(f'Total : {round(pPrice +(pPrice * pValue/100))}원')
-        break
-    elif vAnser == 'n' or 'N':
-        print(f'Total : {pPrice}원')
-        break
-    else :
-        print('부가세 포함 질문에 잘못 입력하셨습니다.')
+pPrice = int(input('소비자 가격(원) : '))
+vAnser = input('부가세 포함입니까? (y/n) : ')
+
+if vAnser == 'y' and pPrice > 0:
+    pValue = int(input('부가세(%) : '))
+    print(f'Total : {round(pPrice +(pPrice * pValue/100))}원')
+
+elif vAnser == 'n' and pPrice > 0:
+    print(f'Total : {pPrice}원')
+
+else :
+    print('가격 또는 부가세 질문에 잘못 입력 하셨습니다.')
